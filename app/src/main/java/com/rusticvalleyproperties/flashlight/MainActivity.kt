ca5c4035fd80c9aaac5fc3b98aa19a994eee2b89
Jesse Spangler <budboy315@gmail.com>
2024-01-27 12:51:53 -0500

    Added capability to adjust flashlight brightness according to android version.

--- app/src/main/java/com/rusticvalleyproperties/flashlight/MainActivity.kt
+++ app/src/main/java/com/rusticvalleyproperties/flashlight/MainActivity.kt
@@ -2,18 +2,32 @@ package com.rusticvalleyproperties.flashlight
 
 import android.content.Context
 import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CameraCharacteristics
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageButton
+import android.widget.SeekBar
+import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import java.lang.IllegalArgumentException
+import androidx.core.view.isVisible
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
-    private lateinit var cameraM :CameraManager
+
+    private lateinit var cameraM:CameraManager
     private lateinit var powerBtn:ImageButton
+    private lateinit var flashlightIntensity:SeekBar
+    private lateinit var cameraPower : TextView
+    private lateinit var tvIntensityText : TextView
+    private lateinit var tvWarn : TextView
     var isFlash = false
+    var intensityPossible = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +36,81 @@ class MainActivity : AppCompatActivity() {
         powerBtn = findViewById(R.id.powerBtn)
         cameraM = getSystemService(Context.CAMERA_SERVICE) as CameraManager
         powerBtn.setOnClickListener { flashlightOnOrOff(it) }
+        cameraPower = findViewById(R.id.tvIntensityLevel)
+        tvIntensityText = findViewById(R.id.tvIntensityText)
+        flashlightIntensity = findViewById(R.id.tvLightIntensity)
+        tvWarn = findViewById(R.id.tvWarning)
+        tvWarn.isVisible = false
+
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intensityPossible = findIntensity()
+
+            if (intensityPossible){
+                val cameraListId = cameraM.cameraIdList[0]
+                val cameraC = cameraM.getCameraCharacteristics(cameraListId)
+                val defaultLevel = cameraC.get(CameraCharacteristics.FLASH_INFO_STRENGTH_DEFAULT_LEVEL)!!
+                val maxlevel = cameraC.get(CameraCharacteristics.FLASH_INFO_STRENGTH_MAXIMUM_LEVEL)
+                cameraPower.text = defaultLevel.toString()
+                cameraPower.isVisible = true
+                flashlightIntensity.isVisible = true
+                tvIntensityText.isVisible = true
+
+                flashlightIntensity.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean
+                    ) {
+                        Log.i(TAG, "on progress changed $progress")
+                        cameraPower.text = "$progress"
+                        Log.i(TAG, "Max strength level: $maxlevel, $defaultLevel")
+                        if(maxlevel!! >= progress) {
+                            cameraM.turnOnTorchWithStrengthLevel(cameraListId, progress)
+                            if(progress > defaultLevel) {
+                                tvWarn.isVisible = true
+                            }else {
+                                tvWarn.isVisible = false
+                            }
+                        }else {
+                            cameraM.turnOnTorchWithStrengthLevel(cameraListId, maxlevel)
+                            tvWarn.isVisible = true
+                        }
+
+                    }
+
+                    override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+                    override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+                })
+            }else {
+                flashlightIntensity.isVisible = false
+                cameraPower.isVisible = false
+                tvIntensityText.isVisible = false
+            }
+        }else {
+            flashlightIntensity.isVisible = false
+            cameraPower.isVisible = false
+            tvIntensityText.isVisible = false
+        }
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private fun findIntensity(): Boolean {
+        val cameraListId = cameraM.cameraIdList[0]
+        val cameraC = cameraM.getCameraCharacteristics(cameraListId)
+        val maxlevel = cameraC.get(CameraCharacteristics.FLASH_INFO_STRENGTH_MAXIMUM_LEVEL)
+        if (maxlevel != null) {
+            if(maxlevel > 1) {
+                flashlightIntensity.max = maxlevel
+                flashlightIntensity.progress = cameraC.get(CameraCharacteristics.FLASH_INFO_STRENGTH_DEFAULT_LEVEL)!!
+                return true
+            }else{
+                return false
+            }
+        }else{
+            return false
+        }
+
+    }
 
     private fun flashlightOnOrOff(v: View?) {
         try {
